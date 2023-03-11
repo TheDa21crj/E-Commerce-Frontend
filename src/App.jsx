c56a158d8d7d2b3c911fa3d4d5b2c2009e445bc7
Rishav Singh <rishavrajcrj@gmail.com>
2023-03-11 20:15:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,34 +24,37 @@ import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Provider store={store}>
-        <Layout>
-          <Nav />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="/Membership" element={<Membership />} /> */}
-            {/* <Route path="/products/:id" element={<ProductsDeatils />} /> */}
-            {/* <Route path="/wishlist" element={<Cart />} /> */}
-            {/* <Route path="/login" element={<Login />} /> */}
-            {/* <Route path="/register" element={<Register />} /> */}
-            {/* <Route path="/logout" element={<Logout />} /> */}
-            {/* <Route exact path="/my-account" element={<PrivateRoute />}> */}
-            {/* <Route exact path="/my-account" element={<MyAccount />} /> */}
-            {/* </Route> */}
-            {/* <Route path="/admin/login" element={<AdminLogin />} /> */}
-            {/* <Route path="/admin/post-content" element={<PostContent />} /> */}
-            {/* <Route path="/ShopCart" element={<ShopCart />} /> */}
-            <Route path="/categories/:gender/:tag" element={<Tag />} />
-            <Route path="/categories/:gender" element={<Gender />} />
-            <Route path="/MERCHANDISE/:link" element={<MerchandiseLink />} />
-            {/* <Route path="/checkout-success" element={<CheckoutSuccess />} /> */}
-            <Route path="*" element={<Error />} />
-          </Routes>
-          <Footer />
-        </Layout>
-      </Provider>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Provider store={store}>
+          <Layout>
+            <Nav />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              {/* <Route path="/Membership" element={<Membership />} /> */}
+              {/* <Route path="/products/:id" element={<ProductsDeatils />} /> */}
+              {/* <Route path="/wishlist" element={<Cart />} /> */}
+              {/* <Route path="/login" element={<Login />} /> */}
+              {/* <Route path="/register" element={<Register />} /> */}
+              {/* <Route path="/logout" element={<Logout />} /> */}
+              {/* <Route exact path="/my-account" element={<PrivateRoute />}> */}
+              {/* <Route exact path="/my-account" element={<MyAccount />} /> */}
+              {/* </Route> */}
+              {/* <Route path="/admin/login" element={<AdminLogin />} /> */}
+              {/* <Route path="/admin/post-content" element={<PostContent />} /> */}
+              {/* <Route path="/ShopCart" element={<ShopCart />} /> */}
+              <Route path="/categories/:gender/:tag" element={<Tag />} />
+              <Route path="/categories/:gender" element={<Gender />} />
+              <Route path="/MERCHANDISE/:link" element={<MerchandiseLink />} />
+              {/* <Route path="/checkout-success" element={<CheckoutSuccess />} /> */}
+              <Route path="*" element={<Error />} />
+            </Routes>
+            <Footer />
+          </Layout>
+        </Provider>
+      </BrowserRouter>
+      <Analytics />
+    </>
   );
 }
 

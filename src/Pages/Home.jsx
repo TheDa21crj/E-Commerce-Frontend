b7fb94e018ad0eff80619c9fd4092ac760630a66
Rishav Singh <rishavrajcrj@gmail.com>
2023-03-11 20:30:15 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -85,7 +85,6 @@ export default function Home() {
             <NewArrival />
             <CATEGORIES />
             <TopSelling />
-            {/* <COLLECTIONS /> */}
             <MERCHANDISE />
           </div>
         )}

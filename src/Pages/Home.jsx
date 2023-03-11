4a920ad59bb32b1dff4df39ba69e975613e40be6
Rishav Singh <rishavrajcrj@gmail.com>
2023-03-11 12:56:03 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,33 @@ export default function Home() {
   useEffect(() => {
     // document.title = "Online Shopping for Men & Women: The Da";
     // dispatch(seeload({ start: "false" }));
-    console.log("first");
+    DataGet();
   }, []);
+
+  const DataGet = async () => {
+    try {
+      const res = await fetch(
+        "https://coral-app-ogkbu.ondigitalocean.app/api/admin/Products/NewArival",
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
+
+      const dataNA = await res.json();
+      if (dataNA.errors) {
+        return console.log("error");
+      }
+      if (dataNA) {
+        console.log(dataNA);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return <div>Home</div>;
 }

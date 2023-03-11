8746a645da659e4c2110bfbc9a4b259e43d8974d
Rishav Singh <rishavrajcrj@gmail.com>
2023-03-11 12:28:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,32 @@ import Error from "./Pages/Error";
 function App() {
   return (
     <BrowserRouter>
-      <Provider store={store}>
-        <Layout>
-          <Nav />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="/Membership" element={<Membership />} /> */}
-            {/* <Route path="/products/:id" element={<ProductsDeatils />} /> */}
-            {/* <Route path="/wishlist" element={<Cart />} /> */}
-            {/* <Route path="/login" element={<Login />} /> */}
-            {/* <Route path="/register" element={<Register />} /> */}
-            {/* <Route path="/logout" element={<Logout />} /> */}
-            {/* <Route exact path="/my-account" element={<PrivateRoute />}> */}
-            {/* <Route exact path="/my-account" element={<MyAccount />} /> */}
-            {/* </Route> */}
-            {/* <Route path="/admin/login" element={<AdminLogin />} /> */}
-            {/* <Route path="/admin/post-content" element={<PostContent />} /> */}
-            {/* <Route path="/ShopCart" element={<ShopCart />} /> */}
-            {/* <Route path="/categories/:gender/:tag" element={<Tag />} /> */}
-            {/* <Route path="/categories/:gender" element={<Gender />} /> */}
-            {/* <Route path="/MERCHANDISE/:link" element={<MerchandiseLink />} /> */}
-            {/* <Route path="/checkout-success" element={<CheckoutSuccess />} /> */}
-            <Route path="*" element={<Error />} />
-          </Routes>
-          <Footer />
-        </Layout>
-      </Provider>
+      {/* <Provider store={store}> */}
+      <Layout>
+        {/* <Nav /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/Membership" element={<Membership />} /> */}
+          {/* <Route path="/products/:id" element={<ProductsDeatils />} /> */}
+          {/* <Route path="/wishlist" element={<Cart />} /> */}
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/register" element={<Register />} /> */}
+          {/* <Route path="/logout" element={<Logout />} /> */}
+          {/* <Route exact path="/my-account" element={<PrivateRoute />}> */}
+          {/* <Route exact path="/my-account" element={<MyAccount />} /> */}
+          {/* </Route> */}
+          {/* <Route path="/admin/login" element={<AdminLogin />} /> */}
+          {/* <Route path="/admin/post-content" element={<PostContent />} /> */}
+          {/* <Route path="/ShopCart" element={<ShopCart />} /> */}
+          {/* <Route path="/categories/:gender/:tag" element={<Tag />} /> */}
+          {/* <Route path="/categories/:gender" element={<Gender />} /> */}
+          {/* <Route path="/MERCHANDISE/:link" element={<MerchandiseLink />} /> */}
+          {/* <Route path="/checkout-success" element={<CheckoutSuccess />} /> */}
+          <Route path="*" element={<Error />} />
+        </Routes>
+        {/* <Footer /> */}
+      </Layout>
+      {/* </Provider> */}
     </BrowserRouter>
   );
 }

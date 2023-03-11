6dc403d4b27bd7ceadaa69d71a8060e2151c18d4
Rishav Singh <rishavrajcrj@gmail.com>
2023-03-11 13:08:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,35 +12,39 @@ import Error from "./Pages/Error";
 import Nav from "./Components/Nav";
 import Footer from "./Components/Footer";
 
+// redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 function App() {
   return (
     <BrowserRouter>
-      {/* <Provider store={store}> */}
-      <Layout>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/Membership" element={<Membership />} /> */}
-          {/* <Route path="/products/:id" element={<ProductsDeatils />} /> */}
-          {/* <Route path="/wishlist" element={<Cart />} /> */}
-          {/* <Route path="/login" element={<Login />} /> */}
-          {/* <Route path="/register" element={<Register />} /> */}
-          {/* <Route path="/logout" element={<Logout />} /> */}
-          {/* <Route exact path="/my-account" element={<PrivateRoute />}> */}
-          {/* <Route exact path="/my-account" element={<MyAccount />} /> */}
-          {/* </Route> */}
-          {/* <Route path="/admin/login" element={<AdminLogin />} /> */}
-          {/* <Route path="/admin/post-content" element={<PostContent />} /> */}
-          {/* <Route path="/ShopCart" element={<ShopCart />} /> */}
-          {/* <Route path="/categories/:gender/:tag" element={<Tag />} /> */}
-          {/* <Route path="/categories/:gender" element={<Gender />} /> */}
-          {/* <Route path="/MERCHANDISE/:link" element={<MerchandiseLink />} /> */}
-          {/* <Route path="/checkout-success" element={<CheckoutSuccess />} /> */}
-          <Route path="*" element={<Error />} />
-        </Routes>
-        <Footer />
-      </Layout>
-      {/* </Provider> */}
+      <Provider store={store}>
+        <Layout>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/Membership" element={<Membership />} /> */}
+            {/* <Route path="/products/:id" element={<ProductsDeatils />} /> */}
+            {/* <Route path="/wishlist" element={<Cart />} /> */}
+            {/* <Route path="/login" element={<Login />} /> */}
+            {/* <Route path="/register" element={<Register />} /> */}
+            {/* <Route path="/logout" element={<Logout />} /> */}
+            {/* <Route exact path="/my-account" element={<PrivateRoute />}> */}
+            {/* <Route exact path="/my-account" element={<MyAccount />} /> */}
+            {/* </Route> */}
+            {/* <Route path="/admin/login" element={<AdminLogin />} /> */}
+            {/* <Route path="/admin/post-content" element={<PostContent />} /> */}
+            {/* <Route path="/ShopCart" element={<ShopCart />} /> */}
+            {/* <Route path="/categories/:gender/:tag" element={<Tag />} /> */}
+            {/* <Route path="/categories/:gender" element={<Gender />} /> */}
+            {/* <Route path="/MERCHANDISE/:link" element={<MerchandiseLink />} /> */}
+            {/* <Route path="/checkout-success" element={<CheckoutSuccess />} /> */}
+            <Route path="*" element={<Error />} />
+          </Routes>
+          <Footer />
+        </Layout>
+      </Provider>
     </BrowserRouter>
   );
 }

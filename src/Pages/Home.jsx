b97eb6f7c90e378c8f134828ebd3eb14072852f7
Rishav Singh <rishavrajcrj@gmail.com>
2023-03-11 12:33:11 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    // document.title = "Online Shopping for Men & Women: The Da";
+    // dispatch(seeload({ start: "false" }));
+    console.log("first");
+  }, []);
   return <div>Home</div>;
 }

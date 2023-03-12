aa446833c04910c991be795fcaf96520db75ce95
Rishav Singh <rishavrajcrj@gmail.com>
2023-03-12 11:12:16 +0530

    Update Tag.jsx

--- src/Pages/Tag.jsx
+++ src/Pages/Tag.jsx
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+
+// Css
 import GenCss from "./Css/Gender.module.css";
 import TSCss from "./../Components/Home/Css/TopSelling.module.css";
+
+// Icons
 import StarIcon from "@mui/icons-material/Star";
 
 export default function Tag() {

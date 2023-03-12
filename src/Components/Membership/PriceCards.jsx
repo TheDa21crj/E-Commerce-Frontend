d3abff8d4161f04fde47b2513d138e1decaca47e
Rishav Singh <rishavrajcrj@gmail.com>
2023-03-12 11:39:43 +0530

    Update PriceCards.jsx

--- src/Components/Membership/PriceCards.jsx
+++ src/Components/Membership/PriceCards.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+
+// Css
 import PCss from "./CSS/PriceCards.module.css";
 
 export default function PriceCards(props) {

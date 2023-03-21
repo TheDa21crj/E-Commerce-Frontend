f88d9b146699f393de129c45768d9a5f3fd0716d
Rishav Singh <rishavrajcrj@gmail.com>
2023-03-21 20:17:52 +0530

    Update CATEGORIES.jsx

--- src/Components/Home/CATEGORIES.jsx
+++ src/Components/Home/CATEGORIES.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import TSCss from "./Css/TopSelling.module.css";
-import CatCss from "./Css/CATEGORIES.css";
+// import CatCss from "./Css/CATEGORIES.css";
 import data from "./JSON/data.json";
 
 export default function CATEGORIES() {
